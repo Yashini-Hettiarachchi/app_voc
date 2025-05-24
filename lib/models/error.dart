4aabdcb7c72ffbd071b1a7831d734a3333940047
Yashini-Hettiarachchi <user@example.com>
2025-05-24 15:05:47 +0530

    Remove all previous project files

--- lib/models/error.dart
+++
@@ -1,6 +0,0 @@
-// TODO remove me
-// ignore_for_file: public_member_api_docs
-
-class InvalidImageException implements Exception {
-  InvalidImageException();
-}
--- main.py
+++
@@ -1,7 +0,0 @@
-# Import the FastAPI app from the backend module
-from backend.main import app
-
-# This file is needed for Elastic Beanstalk to find the application
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
